1c115427fc76027ee345b1db4a0ad1a2a378c88a
Luc Deschenaux <luc.deschenaux@freesurf.ch>
2019-02-06 14:16:47 +0700

    initial commit

---
+++ client/app/js/app.js
@@ -0,0 +1,100 @@
+/*
+* Copyright (c) 2018 ALSENET SA
+*
+* Author(s):
+*
+*      Luc Deschenaux <luc.deschenaux@freesurf.ch>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+'use strict';
+
+module.exports=[
+  '$rootScope',
+  '$state',
+  '$stateParams',
+  '$window',
+  '$transitions',
+
+  function (
+    $rootScope,
+    $state,
+    $stateParams,
+    $window,
+    $transitions
+  ) {
+
+    $rootScope.config=require('../../../config.json');
+
+    $rootScope.$on('showOverlay',function(event,options){
+      if ($window.parent) {
+        $window.parent.postMessage({type: 'showOverlay', options: JSON.stringify(options)}, $window.document.location.origin);
+      }
+    });
+
+    $rootScope.$on('hideOverlay',function(event,options){
+      if ($window.parent) {
+        $window.parent.postMessage({type: 'hideOverlay', options: JSON.stringify(options)}, $window.document.location.origin);
+      }
+    });
+
+    // allow parent window or iframe to set the current state
+    $window.addEventListener('message',receiveMessage,false);
+    function receiveMessage(msg){
+      if (msg.origin!=$window.document.location.origin) {
+        console.log(msg);
+        return;
+      }
+      if (msg.data && msg.data.type) {
+        console.log(msg.data.type);
+        switch(msg.data.type){
+          case 'transition':
+            $state.transitionTo(msg.data.toState, msg.data.stateParams||{}, {reload: msg.data.reload});
+            break;
+          default:
+            console.log('unhandled message', msg);
+            break;
+        }
+      } else {
+        console.log('unhandled message', msg);
+      }
+    }
+
+    // share $state and $stateParams with child scopes
+    $rootScope.$state=$state;
+    $rootScope.$stateParams=$stateParams;
+
+    // running in cordova ?
+    $rootScope.mobileApp=['http:','https:'].indexOf($window.document.location.protocol)<0;
+
+    $transitions.onSuccess({}, function(transition) {
+      // set window title on state change
+      $rootScope.title = transition.to().title;
+      // allow state related css rules
+      $('body').removeClass('_'+transition.from().name.replace(/\./g,'_').replace(/([A-Z])/g,function(all,letter){return '-'+letter.toLowerCase()})).addClass('_'+transition.to().name.replace(/\./g,'_').replace(/([A-Z])/g,function(all,letter){return '-'+letter.toLowerCase()}));
+      // notify parent window
+      if ($window.parent) {
+        $window.parent.postMessage({type: 'transitionSuccess', toState: transition.to().name}, $window.document.location.origin);
+      }
+    });
+    $transitions.onStart({}, function(transition) {
+      // notify parent window
+      if ($window.parent) {
+        $window.parent.postMessage({type: 'transitionStart', toState: transition.to().name}, $window.document.location.origin);
+      }
+    });
+  }
+];
---
+++ client/app/js/index.js
@@ -0,0 +1,64 @@
+/*
+* Copyright (c) 2018 ALSENET SA
+*
+* Author(s):
+*
+*      Luc Deschenaux <luc.deschenaux@freesurf.ch>
+*
+* This program is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Affero General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Affero General Public License for more details.
+*
+* You should have received a copy of the GNU Affero General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*
+*/
+
+'use strict';
+
+// force https... could use meta
+if (window.location.protocol!='https:') {
+  var href=window.location.href.replace(/[^:]+/,'https');
+  window.location.assign(href);
+}
+
+var angular=require('angular');
+window.jQuery=window.$=require('jquery');
+
+var config=require('../../../config.json');
+
+$('html').attr('ng-app',config.appName);
+$('title').text(config.appName);
+
+// TODO: (?) generate the code below from ../../../gulpfile.js using ../../../config.json and directories content.
+require('../css/bootstrap.min.css');
+require('../css/main.css');
+
+require('popper.js');
+require('bootstrap');
+require('@uirouter/angularjs/release/angular-ui-router.js');
+require('angular-ui-bootstrap');
+
+var app=angular.module(config.appName,[
+  'ui.router',
+  'ui.bootstrap'
+])
+
+angular.module(config.appName)
+.config(require('./config.js'))
+.run(require('./app.js'))
+
+require('./scripts.js')(app,config);
+
+//.service('myService',require('./services/my-service.js'))
+//.directive('my-directive',require('./directives/my-directive.js'))
+//.controller('MainCtrl',require('./controllers/main.js'))
+//.controller('HomeCtrl',require('./controllers/home.js'))
+//.controller('OverlayCtrl',require('./controllers/overlay.js'))
+//.filter('my-filter',require('./filters/my-filter.js'))
